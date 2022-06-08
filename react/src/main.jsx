3babaed047ee24a0adebbb6288635893658335f3
Peter Weber <pwweber@gmail.com>
2022-06-08 16:16:18 -0600

    Add alternate link, include newest client for async loop

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -1,11 +1,15 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import Client from 'applura/client';
+import { bootstrap } from 'client';
 const container = document.getElementById('app');
 console.assert(container);
 
-const client = new Client();
-for await (const { resource, problem } of client.start()) {
-  ReactDOM.hydrateRoot(container, <App resource={resource} problem={problem} client={client} />);
+const client = bootstrap();
+const init = async () => {
+  for await (const { resource, problem } of client.start()) {
+    ReactDOM.hydrateRoot(container, <App resource={resource} problem={problem} client={client} />);
+  }
 }
+// To avoid top level await.
+init();
