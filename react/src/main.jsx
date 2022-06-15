ed333e40b58f0bf0de03e30a0eed1df1c8e50687
Peter Weber <peter@applura.com>
2022-06-15 14:44:54 -0600

    Fix render loop to avoid creating root multiple times

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -1,4 +1,4 @@
-import ReactDOM from 'react-dom/client'
+import { createRoot, hydrateRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { bootstrap } from 'client';
@@ -7,8 +7,16 @@ console.assert(container);
 
 const client = bootstrap();
 const init = async () => {
+  let root = null;
+  const render = (component) => {
+    if (!root && container.dataset.appluraRendered) {
+      root = hydrateRoot(container, component);
+    } else {
+      (root ?? (root = createRoot(container))).render(component);
+    }
+  }
   for await (const { resource, problem } of client.start()) {
-    ReactDOM.hydrateRoot(container, <App resource={resource} problem={problem} client={client} />);
+    render(<App resource={resource} problem={problem} client={client} />);
   }
 }
 // To avoid top level await.
