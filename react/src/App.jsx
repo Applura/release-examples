ec69fac2480b8bf87d73b9bf217b321c73b3bb05
Peter Weber <peter@applura.com>
2022-06-15 17:11:38 -0600

    Refactor barrel into router like component

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
-import Page from './pages/Page';
+import MatchResource from './pages/MatchResource';
+import ArticlePage from './pages/ArticlePage';
+import BasicPage from './pages/BasicPage';
+import HomePage from './pages/HomePage';
+import CollectionPage from './pages/CollectionPage';
+import DefaultPage from './pages/DefaultPage';
 import { ResourceProvider } from './ResourceContext';
-function App({ resource, problems, client, loading }) {
+function App({ resource, problems, loading, client }) {
 
   return (
     <React.StrictMode>
       <ResourceProvider resource={resource} problems={problems} client={client}>
-        {resource && <Page type={resource.type}>
-
-        </Page>}
+        <MatchResource resource={resource}>
+          <HomePage type="home" />
+          <BasicPage type="page" />
+          <ArticlePage type="article" />
+          <CollectionPage type="collection" />
+          <DefaultPage default />
+        </MatchResource>
         {loading && <p>Loading...</p>}
       </ResourceProvider>
     </React.StrictMode>
