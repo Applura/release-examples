ccaddd85e38088593562ec5bf64ee159eabbf5db
Peter Weber <peter@applura.com>
2022-06-15 16:40:49 -0600

    Reorganize file

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -2,21 +2,22 @@ import { createRoot, hydrateRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { bootstrap } from 'client';
-const container = document.getElementById('app');
-console.assert(container);
 
-const client = bootstrap();
+const container = document.getElementById('app');
+
 const init = async () => {
-  let root = null;
-  const render = (component) => {
-    if (!root && container.dataset.appluraRendered) {
-      root = hydrateRoot(container, component);
-    } else {
-      (root ?? (root = createRoot(container))).render(component);
-    }
-  }
+  const client = bootstrap();
   for await (const { resource, problem, loading } of client.start()) {
     render(<App resource={resource} problem={problem} loading={loading} client={client} />);
+  }
+}
+
+let root = null;
+const render = (component) => {
+  if (!root && container.dataset.appluraRendered) {
+    root = hydrateRoot(container, component);
+  } else {
+    (root ?? (root = createRoot(container))).render(component);
   }
 }
 // To avoid top level await.
