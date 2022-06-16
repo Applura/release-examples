83c01175cbd852704077d26f05db9f97bbefbe11
Gabriel SULLICE <gabriel@sullice.com>
2022-06-16 10:49:35 -0600

    add missing "loading" prop

--- react/src/glue/internal/contexts/ResourceContext.jsx
+++ react/src/glue/internal/contexts/ResourceContext.jsx
@@ -2,10 +2,10 @@ import { createContext } from 'react';
 
 export const ResourceContext = createContext(null);
 
-export const ResourceProvider = ({ resource, problem, client, children }) => {
+export const ResourceProvider = ({ resource, problem, loading, client, children }) => {
   const follow = client && client.hasOwnProperty('follow') ? client.follow : () => {};
   return (
-    <ResourceContext.Provider value={{ resource, problem, follow }}>
+    <ResourceContext.Provider value={{ resource, problem, loading, follow }}>
       {children}
     </ResourceContext.Provider>
   )
